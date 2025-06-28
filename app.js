081c2b15ddeaac3ba6187cc896fee54b72d6a49a
Iqra Rehman <rehmaniqra22@gmail.com>
2025-06-28 09:52:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,151 @@
+// Write a program that takes a positive integer from user &
+// display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+
+//  var userInput = prompt("Enter a positive-integer:");
+
+//     var number = parseFloat(userInput);
+
+//     var roundValue = Math.round(number);
+//     var floorValue = Math.floor(number);
+//     var ceilValue = Math.ceil(number);
+
+
+//     document.write("<h3>Number: " + number + "</h3>");
+//     document.write("<h3>Round off value: " + roundValue + "</h3>");
+//     document.write("<h3>Floor value: " + floorValue + "</h3>");
+//     document.write("<h3>Ceil value: " + ceilValue + "</h3>");
+
+
+
+// 2. Write a program that takes a negative floating point
+// number from user & display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+//  var userInput = prompt("Enter a negative floating-point number:");
+
+//     var number = parseFloat(userInput);
+
+//     var roundValue = Math.round(number);
+//     var floorValue = Math.floor(number);
+//     var ceilValue = Math.ceil(number);
+
+//     document.write("<h3>Number: " + number + "</h3>");
+//     document.write("<h3>Round off value: " + roundValue + "</h3>");
+//     document.write("<h3>Floor value: " + floorValue + "</h3>");
+//     document.write("<h3>Ceil value: " + ceilValue + "</h3>");
+
+
+
+
+
+
+// 3. Write a program that displays the absolute value of a
+// number.
+// E.g. absolute value of -4 is 4 & absolute value of 5 is 5
+
+
+// var userInput = prompt("Enter the absolute value of a number");
+// var number = parseFloat(userInput);
+ 
+// var absoluteValue = Math.abs(number);
+// document.write("<h3>Number: " + number + "</h3>");
+// document.write("<h3>Absolute Value :" + absoluteValue + "</h3>");
+
+
+
+
+// 4. Write a program that simulates a dice using random()
+// method of JS Math class. Display the value of dice in your
+// browser.:
+
+//     var diceValue = Math.floor(Math.random() *6) + 1;
+
+
+//   document.write("<h3>Dice value: " + diceValue + "</h3>");
+
+
+
+
+
+
+// 5. Write a program that simulates a coin toss using random()
+// method of JS Math class. Display the value of coin in your
+// browser
+
+// var toss = Math.floor(Math.random() * 2);
+
+//     var result;
+
+   
+//     if (toss === 0) {
+//       result = "Heads";
+//     } else {
+//       result = "Tails";
+//     }
+
+    
+//     document.write("<h3>Coin Toss Result: " + result + "</h3>");
+
+
+
+
+
+
+// 6. Write a program that shows a random number between 1
+// and 100 in your browser.
+
+
+// var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+   
+//     document.write("<h3>Random Number (1 to 100): " + randomNumber + "</h3>")
+
+
+
+
+
+// 7. Write a program that asks the user about his weight. Parse
+// the user input and display his weight in your browser.
+// Possible user inputs can be:
+// a. 50
+// b. 50kgs
+// c. 50.2kgs
+// d. 50.2kilograms
+
+//  var userInput = prompt("Enter your weight (e.g. 50, 50kgs, 50.2kgs, 50.2kilograms):");
+
+//     var weight = parseFloat(userInput);
+
+//     document.write("<h3>Your weight is: " + weight + " kilograms</h3>");
+
+
+
+
+
+
+// 8. Write a program that stores a random secret number from
+// 1 to 10 in a variable. Ask the user to input a number
+// between 1 and 10. If the user input equals the secret
+// number, congratulate the user.
+
+
+
+
+// var secretNumber = Math.floor(Math.random() * 10) + 1;
+// var userGuess = parseInt(prompt("Guess the secret number (between 1 and 10): "));
+ 
+//  if (userGuess === secretNumber) {
+//       document.write("<h3>Congratulations! You guessed the correct number: " + secretNumber + "</h3>");
+//     } else {
+//       document.write("<h3> Sorry, try again: " + secretNumber + "</h3>");
+//     }
+
+
